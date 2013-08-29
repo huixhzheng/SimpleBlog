baa8756ddd78f900e934909b7141ea1f7a42db73
matt-duell <mkduell@gmail.com>
2013-08-28 23:04:26 -0400

    enhancing startup script

--- SimpleBlog/restartTomcat.sh
+++ SimpleBlog/restartTomcat.sh
@@ -1,12 +1,13 @@
 tomcatPID=`ps -ef | grep java | grep tomcat | sort | cut -f3 -d' '`
 tomcatPIDTwo=`ps -ef | grep java | grep tomcat | sort | cut -f2 -d' '`
+TOMCAT_HOME=/home/duellmat/bin/apache-tomcat-6.0.37
 kill -9 $tomcatPID
 kill -9 $tomcatPIDTwo
 
 if [ "$1" = "debug" ]; then
 	export JPDA_ADDRESS=8000
 	export JPDA_TRANSPORT=dt_socket
-	sh /home/duellmat/dev/bin/apache-tomcat-6.0.37/bin/catalina.sh jpda start
+	sh $TOMCAT_HOME/bin/catalina.sh jpda start
 	exit 0
 fi
-sh /home/duellmat/dev/bin/apache-tomcat-6.0.37/bin/startup.sh
+sh $TOMCAT_HOME/bin/startup.sh
