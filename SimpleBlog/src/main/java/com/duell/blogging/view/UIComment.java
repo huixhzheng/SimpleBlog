e97078f086e60cb13d10cb92829b84ec780e9e9a
matt-duell <mkduell@gmail.com>
2013-11-14 00:27:16 -0500

    Hashcode and equals

--- SimpleBlog/src/main/java/com/duell/blogging/view/UIComment.java
+++ SimpleBlog/src/main/java/com/duell/blogging/view/UIComment.java
@@ -36,6 +36,55 @@ public class UIComment {
 		this.parentBlogId = parentBlogId;
 	}
 	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result
+				+ ((commenterEmail == null) ? 0 : commenterEmail.hashCode());
+		result = prime * result
+				+ ((commenterName == null) ? 0 : commenterName.hashCode());
+		result = prime * result
+				+ ((parentBlogId == null) ? 0 : parentBlogId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UIComment other = (UIComment) obj;
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (commenterEmail == null) {
+			if (other.commenterEmail != null)
+				return false;
+		} else if (!commenterEmail.equals(other.commenterEmail))
+			return false;
+		if (commenterName == null) {
+			if (other.commenterName != null)
+				return false;
+		} else if (!commenterName.equals(other.commenterName))
+			return false;
+		if (parentBlogId == null) {
+			if (other.parentBlogId != null)
+				return false;
+		} else if (!parentBlogId.equals(other.parentBlogId))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "UIComment [commenterName=" + commenterName
+				+ ", commenterEmail=" + commenterEmail + ", comment=" + comment
+				+ ", parentBlogId=" + parentBlogId + "]";
+	}
 	
 }
