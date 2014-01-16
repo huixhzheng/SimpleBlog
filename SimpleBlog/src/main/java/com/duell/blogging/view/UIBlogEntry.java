100efa8fd6aa5824f6673513e979782a1241fd09
matt-duell <mkduell@gmail.com>
2014-01-16 18:09:01 -0500

    Some possible changes to support streaming an image across a webservice

--- SimpleBlog/src/main/java/com/duell/blogging/view/UIBlogEntry.java
+++ SimpleBlog/src/main/java/com/duell/blogging/view/UIBlogEntry.java
@@ -1,22 +1,14 @@
 package com.duell.blogging.view;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Set;
 
 public class UIBlogEntry {
-	
-	public UIBlogEntry()
-	{}
-	public UIBlogEntry(UIBlogEntry that)
-	{
-		this.id=that.id;
-		this.title=that.title;
-		this.content=that.content;
-		this.author=that.author;
-		this.author_contact=that.author_contact;
-		this.publish_date=that.publish_date;
-	}
 	
 	private Integer id;
 
@@ -33,7 +25,86 @@ public class UIBlogEntry {
 	private Collection<UITag> tags;
 	
 	private Collection<UIComment> comments;
+	
+	private byte[] imageBytes;
+	
+	private String imageURI = null;
+	
+	public UIBlogEntry()
+	{
+		imageURI = "SimpleBlog/images/lakemountain_532_290.png";
+	}
+	public UIBlogEntry(UIBlogEntry that)
+	{
+		this.id=that.id;
+		this.title=that.title;
+		this.content=that.content;
+		this.author=that.author;
+		this.author_contact=that.author_contact;
+		this.publish_date=that.publish_date;
+		
+		
+		File f = new File("SimpleBlog/images/lakemountain_532_290.png");
+		long size = f.getTotalSpace();
+		imageBytes = new byte[(int)size];
+		
 
+		FileInputStream fis = null;
+		try
+		{
+			fis = new FileInputStream(f);
+			fis.read(imageBytes);
+			
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			imageBytes = null;
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			imageBytes = null;
+		}
+		finally
+		{
+			try
+			{
+				if(fis!=null)
+				{
+					fis.close();
+				}
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}	
+	
+	public byte[] getImageBytes()
+	{
+		return imageBytes;
+	}
+	
+	public void setImageBytes(byte[] imageBytes)
+	{
+		this.imageBytes = imageBytes;
+	}
+	
+	
+	public String getImageURI()
+	{
+		return imageURI;
+	}
+	
+	public void setImageURI(String imageURI)
+	{
+		this.imageURI = imageURI;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -92,6 +163,13 @@ public class UIBlogEntry {
 	}
 	public void setComments(Collection<UIComment> comments) {
 		this.comments = comments;
+	}
+	@Override
+	public String toString()
+	{
+		return "UIBlogEntry [id=" + id + ", title=" + title + ", content=" + content + ", author=" + author
+				+ ", author_contact=" + author_contact + ", publish_date=" + publish_date + ", tags=" + tags + ", comments="
+				+ comments + ", imageBytes=" + Arrays.toString(imageBytes) + ", imageURI=" + imageURI + "]";
 	}
 	
 	
